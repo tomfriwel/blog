8f34a341285bacdf43e7cdd215795fb5fa49ef5c
tomfriwel <976932447@qq.com>
2018-05-03 10:21:51 +0800

    new script and add

---
+++ _drafts/small-target-2018.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "title"
+excerpt: >-
+    excerpt
+comments: true
+---
+
--- scripts.py
+++ scripts.py
@@ -2,17 +2,29 @@
 # coding:utf-8
 
 import os
+import sys
 import argparse
 import subprocess
 from datetime import datetime
 import time
 
+# date
+
+
+def getDate(format='%Y-%m-%d %H:%M:%S'):
+    date_object = datetime.now()
+    current_time = date_object.strftime(format)
+    return current_time
+
 # clipboard
+
+
 def getClipboardData():
     p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
     retcode = p.wait()
     data = p.stdout.read()
     return data
+
 
 def setClipboardData(data):
     p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
@@ -20,12 +32,12 @@ def setClipboardData(data):
     p.stdin.close()
     retcode = p.wait()
 
+
 def saveCurrentTimeToCp():
-    date_object = datetime.now()
-    current_time = date_object.strftime('%Y-%m-%d %H:%M:%S')
-    setClipboardData(current_time)
+    setClipboardData(getDate())
 
 # file info
+
 
 def getFileInfo(path):
     mtime = os.path.getmtime(path)
@@ -35,35 +47,62 @@ def getFileInfo(path):
     print(formatTimestamp(os.stat(path).st_mtime))
     print(formatTimestamp(os.stat(path).st_ctime))
 
+
+def newDraft(filename):
+    arr = filename.split()
+    filename = '-'.join(arr)
+    filename = './_drafts/' + filename + '.md'
+    try:
+        file = open(filename, 'w')   # Trying to create a new file or open one
+        file.write("""---
+layout: post
+title:  "title"
+excerpt: >-
+    excerpt
+comments: true
+---
+
+""")
+        file.close()
+        print('create '+filename)
+    except:
+        print('Something went wrong! Can\'t tell what?')
+        sys.exit(0)  # quit Python
+
 # format timestamp
+
+
 def formatTimestamp(timestamp):
     print(timestamp)
     time_local = time.localtime(timestamp)
-    #转换成新的时间格式(2016-05-05 20:28:54)
+    # 转换成新的时间格式(2016-05-05 20:28:54)
     dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
 
     print(dt)
 
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--type", required=True,
-	help="which type")
+                help="which type")
 ap.add_argument("-e", "--extra", required=False,
-	help="extra param")
+                help="extra param")
 args = vars(ap.parse_args())
 
 
 def switch(case, extra=None):
     states = {
-        'savedate':saveCurrentTimeToCp,
-        'fileinfo':getFileInfo
+        'savedate': saveCurrentTimeToCp,
+        'fileinfo': getFileInfo,
+        'newdraft': newDraft
     }
     if states.has_key(case):
         if extra is not None:
-             states[case](extra)
+            states[case](extra)
         else:
             states[case]()
-        
+
     else:
-        print('no key:'+case)
+        print('no key:' + case)
+
 
 switch(args['type'], args['extra'])
